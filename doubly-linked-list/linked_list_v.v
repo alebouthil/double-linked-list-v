f44f666878249cd3d164ff4df253ae18b83f07aa
Alexander <lebo8020@mylaurier.ca>
2024-05-06 23:26:32 -0400

    added readme

--- doubly-linked-list/linked_list_v.v
+++ doubly-linked-list/linked_list_v.v
@@ -1,38 +1,61 @@
-module linked_list
+module doubly_linked_list
+
 // an attempt at implimenting a linked list in the v language
+type returntype = []int | []string | []u8 | int | string | u8
 
-pub struct Linked_List{
-	mut:
-		tail &Node
-		head &Node
-		length int
-
+pub struct Linked_List {
+mut:
+	tail   &Node
+	head   &Node
+	length int
 }
 
-struct Node[T]{
+struct Node[T] {
+mut:
+	next  &Node = unsafe { nil }
+	prev  &Node = unsafe { nil }
 	value T
-	next &Node
-	prev &Node
 }
 
-pub fn (l Linked_List)len(){
-	//getter
+pub fn (l Linked_List) get_head() returntype {
+	// returns value of node at head of list
+	return l.head.value
+}
+
+pub fn (l Linked_List) get_tail() returntype {
+	// returns value of node at tail of list
+	return l.tail.value
+}
+
+pub fn (l Linked_List) len() {
+	// getter for list length
 	return l.length
 }
 
-pub fn (l Linked_List)push<T>(value T){
-	x := Node{
-		value : value
-		prev : l.tail
+pub fn (l Linked_List) push[T](value T) {
+	// push new value onto rear of list
+	if l.length == 0 {
+		x := Node{
+			value: value
 		}
-	l.tail.next = x
-	l.tail = x
-	l.length += 1
+		l.head = x
+		l.tail = x
+	} else {
+		x := Node{
+			value: value
+			prev: l.tail
+		}
+
+		l.tail.next = x
+		l.tail = x
+		l.length += 1
+	}
 }
-pub fn (l Linked_List)pop() [T]{
-//returns value of last node and deletes it
-	x := Node{
-	value : tail.value
-	prev : tail.prev
-	next : }
+
+pub fn (l Linked_List) pop() returntype {
+	// returns value of last node and deletes it
+
+	if l.length == 0 {
+	}
+	value := l.tail
 }
--- doubly-linked-list/readme.txt
+++ doubly-linked-list/readme.txt
@@ -1,3 +1,3 @@
-A simple linked list created in the v programming language
+A simple doubly linked list created in the v programming language as an educational excersise
 usage:
 add the folder linked_list_v to your project directory, and import with "import linked_list_v"
